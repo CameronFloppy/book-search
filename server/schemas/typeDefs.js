6a2d4d57b649531d9ff254fd53eb36f99639045b
Jay Ford <jay.ford45@gmail.com>
2022-07-05 14:08:54 -0400

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!):Auth
+        addUser(username: String!, email: String!, password: String!):Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth  {
+        token: ID!
+        user: User
+    }
+`
